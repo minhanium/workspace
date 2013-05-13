c57b8448136f19f42e4f0ec71cac5b48ee100de7
Tran Phong Phu <vietean@gmail.com>
2013-05-13 18:36:46 +0700

    - Allow to verify production s3

--- util-helper-scripts/younet/verify-apps-on-production.py
+++ util-helper-scripts/younet/verify-apps-on-production.py
@@ -1,4 +1,18 @@
 import os;
-cloud_app_path = '/home/phutp/workspace/cloud.younetco.com';
-list_apps = [ name for name in os.listdir(cloud_app_path) if os.path.isdir(os.path.join(cloud_app_path, name)) ];
-print list_apps;
+#cloud_app_path = '/home/phutp/workspace/cloud.younetco.com'
+cloud_app_path = '.'
+list_apps = [ name for name in os.listdir(cloud_app_path) if os.path.isdir(os.path.join(cloud_app_path, name)) ]
+
+for app_name in list_apps:
+	if app_name not in ['.svn', 's3', 'libraries']:
+		app_path = os.path.join(cloud_app_path, app_name)
+		config_path = os.path.join(app_path,'application/configs')
+		application_ini_file = os.path.join(config_path, 'application.ini')
+		try:
+			if 'i2YIM0Jh28DcNBKyhxUClsOAITXSgXSVIS0OvO1q' in open(application_ini_file).read():
+				print app_name + ":true" 
+			else:
+				print app_name + ":FAILE"
+		except: 
+			print "Can not get config of " + app_name
+
